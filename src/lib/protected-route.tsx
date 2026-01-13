4ccd306ef87b6ee2fe5b7b238c11c6286fee121e
= <topghostly@gmail.com>
2026-01-13 16:46:59 +0100

    fix: 404 error bug; remove loading on load

--- src/lib/protected-route.tsx
+++ src/lib/protected-route.tsx
@@ -2,7 +2,7 @@ import { useMe } from "@/quaries/useMe";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isLoading, isError, status } = useMe();
+  const { isError, status } = useMe();
 
   // if (isLoading) return <div>Loading...</div>;
 
